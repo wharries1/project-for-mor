9925e773573831f1d087c9326c0759c1c78a0a08
Will Harries <williamharries92@gmail.com>
2023-03-10 16:30:53 +0000

    commit message

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
     proxy: true,
-    url: env('MY_HEROKU_URL'), // Sets the public URL of the application.
+    url: env('APP_URL'), // Sets the public URL of the application.
     app: { 
       keys: env.array('APP_KEYS')
     },
