d6d0f4d1d1f837652865bf5234456c9bc3c3eb7e
Will Harries <williamharries92@gmail.com>
2023-03-16 12:07:22 +0000

    commit

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,58 @@
-'use strict';
+"use strict";
+const stripe = require("stripe")(sk_test_51MeKA0Ao8puVP1WfxdlT8TEJRWccDzcPdzlStBXdbrdWq56DSeFD21Op1tOtPPlKqMuZYabosJgpI5DdOXr253PK00kjzDJiyx);
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products, userName, email } = ctx.request.body;
+    try {
+      // retrieve item information
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::item.item")
+            .findOne(product.id);
+
+          return {
+            price_data: {
+              currency: "gbp",
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: product.count,
+          };
+        })
+      );
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        customer_email: email,
+        mode: "payment",
+        success_url: "http://localhost:3000/checkout/success",
+        cancel_url: "http://localhost:3000",
+        line_items: lineItems,
+      });
+      
+      console.log("Session ID:", session.id);
+      
+      await strapi
+        .service("api::order.order")
+        .create({ data: {email, userName, products, stripeSessionId: session.id } });
+      
+      console.log("Order created successfully");
+      
+      // return the session id
+      return { id: session.id };
+    } catch (error) {
+      ctx.response.status = 500;
+      return { error: { message: "There was a problem creating the charge" } };
+    }
+  },
+}));
